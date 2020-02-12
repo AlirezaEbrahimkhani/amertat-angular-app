2daca9888f89771022626b90d0cc7d4ce81a513e
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-12 16:05:07 +0330

    import matrial module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  exports : [
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
