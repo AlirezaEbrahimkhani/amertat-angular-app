b191dc939e89907c3c13696a9331f2dcd195670c
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-14 11:03:24 +0330

    about-us component created

--- src/app/site-information/site-information.module.ts
+++ src/app/site-information/site-information.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SiteInformationRoutingModule } from './site-information-routing.routes';
 import { SiteInformationRouringComponent } from './components/site-information-rouring/site-information-rouring.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 
 @NgModule({
-  declarations: [SiteInformationRouringComponent],
+  declarations: [
+    SiteInformationRouringComponent, 
+    AboutUsComponent
+  ],
   imports: [
     CommonModule,
     SiteInformationRoutingModule
