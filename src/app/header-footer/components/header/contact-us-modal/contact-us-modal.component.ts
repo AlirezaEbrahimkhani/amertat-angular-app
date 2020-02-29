b95147d073363336c7ab3f8acb690f7380132d3d
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-29 23:54:51 +0330

    contact us modal created

---
+++ src/app/header-footer/components/header/contact-us-modal/contact-us-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-modal',
+  templateUrl: './contact-us-modal.component.html',
+  styleUrls: ['./contact-us-modal.component.scss']
+})
+export class ContactUsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/header-footer/header-footer.module.ts
+++ src/app/header-footer/header-footer.module.ts
@@ -4,19 +4,22 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatButtonModule } from '@angular/material';
 import { HeaderDialogComponent } from './components/header/header-dialog/header-dialog.component';
+import { ContactUsModalComponent } from './components/header/contact-us-modal/contact-us-modal.component';
+import { MaterialModule } from '../shared/material.module';
 
 
 @NgModule({
   declarations: [
     HeaderComponent, 
     FooterComponent, 
-    NavbarComponent, HeaderDialogComponent
+    NavbarComponent, 
+    HeaderDialogComponent, 
+    ContactUsModalComponent,
   ],
   imports: [
     CommonModule,
-    MatButtonModule,
+    MaterialModule,
   ],
   exports : [
     HeaderComponent, 
