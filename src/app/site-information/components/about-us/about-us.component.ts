e33d1922eb912a1d339a653cbec30c2eca4a02de
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-11 23:57:04 +0330

    about us page completed

--- src/app/site-information/components/about-us/about-us.component.ts
+++ src/app/site-information/components/about-us/about-us.component.ts
@@ -13,6 +13,17 @@ export class AboutUsComponent implements OnInit {
     "Lorem ipsum dolor sit amet consectetur adipisicing elit.Cupiditate, iure incidunt aspernatur officiis perferendis repudiandae."
   ];
 
+  persons : string[] = [
+    "person1.jpg",
+    "person2.jpg",
+    "person3.jpg",
+    "person4.jpg",
+    "person1.jpg",
+    "person2.jpg",
+    "person3.jpg",
+    "person4.jpg",
+  ]
+
   constructor() {}
 
   ngOnInit() {}
