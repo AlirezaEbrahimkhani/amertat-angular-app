a3a0950ef4023e713fd9a83c6be24d42e2be4d52
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-14 22:47:38 +0330

    PopularDestinationComponent created

---
+++ src/app/shared/components/popular-destination/popular-destination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-destination',
+  templateUrl: './popular-destination.component.html',
+  styleUrls: ['./popular-destination.component.scss']
+})
+export class PopularDestinationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
