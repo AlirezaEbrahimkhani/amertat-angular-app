adcda429dc998d76dfd7bd865776bc3eacf1eff1
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-14 23:29:14 +0330

    popular destination compoent completed

--- src/app/shared/components/popular-destination/popular-destination.component.ts
+++ src/app/shared/components/popular-destination/popular-destination.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-popular-destination',
-  templateUrl: './popular-destination.component.html',
-  styleUrls: ['./popular-destination.component.scss']
+  selector: "app-popular-destination",
+  templateUrl: "./popular-destination.component.html",
+  styleUrls: ["./popular-destination.component.scss"]
 })
 export class PopularDestinationComponent implements OnInit {
+  cardNumber: string[] = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight"
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
