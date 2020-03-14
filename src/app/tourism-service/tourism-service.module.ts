2305e103a9f1a4fd60fead4a08670e9aaa4146c3
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-14 23:30:22 +0330

    import shared module

--- src/app/tourism-service/tourism-service.module.ts
+++ src/app/tourism-service/tourism-service.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TourismServiceRoutingComponent } from './components/tourism-service-routing/tourism-service-routing.component';
 import { TourismServiceRoutingModule } from './tourism-service-routing.routes';
 import { TourismServiceComponent } from './components/tourism-service/tourism-service.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [TourismServiceRoutingComponent, TourismServiceComponent],
   imports: [
     CommonModule,
-    TourismServiceRoutingModule
+    TourismServiceRoutingModule,
+    SharedModule
   ]
 })
 export class TourismServiceModule { }
