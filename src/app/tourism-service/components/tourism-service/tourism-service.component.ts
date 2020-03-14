be89e3af7648f1764d6eb96dcfcf5b0c43383bbd
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-14 22:34:38 +0330

    tourism service compoenent created

---
+++ src/app/tourism-service/components/tourism-service/tourism-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tourism-service',
+  templateUrl: './tourism-service.component.html',
+  styleUrls: ['./tourism-service.component.scss']
+})
+export class TourismServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tourism-service/tourism-service.module.ts
+++ src/app/tourism-service/tourism-service.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TourismServiceRoutingComponent } from './components/tourism-service-routing/tourism-service-routing.component';
 import { TourismServiceRoutingModule } from './tourism-service-routing.routes';
+import { TourismServiceComponent } from './components/tourism-service/tourism-service.component';
 
 
 @NgModule({
-  declarations: [TourismServiceRoutingComponent],
+  declarations: [TourismServiceRoutingComponent, TourismServiceComponent],
   imports: [
     CommonModule,
     TourismServiceRoutingModule
