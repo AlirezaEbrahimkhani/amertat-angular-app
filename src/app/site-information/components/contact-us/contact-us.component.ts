4a3f64386872045ee833106192c8dde2c235b4bd
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-14 22:13:34 +0330

    feedback field options

--- src/app/site-information/components/contact-us/contact-us.component.ts
+++ src/app/site-information/components/contact-us/contact-us.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  selector: "app-contact-us",
+  templateUrl: "./contact-us.component.html",
+  styleUrls: ["./contact-us.component.scss"]
 })
 export class ContactUsComponent implements OnInit {
+  feedBacks: string[] = [
+    "haji namosan karet doroste = 5",
+    "haji karet doroste = 4",
+    "haji karet okeye = 3",
+    "haji karet yekam gir dare = 2",
+    "haji razi nistam = 1",
+    "haji namosan gand zadi = 0"
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
