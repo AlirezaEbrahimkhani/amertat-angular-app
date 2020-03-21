0a55c9eba9dbc8992b44cca9e67af6e49337edae
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-21 13:54:04 +0430

    add blog component to declaration

--- src/app/site-information/site-information.module.ts
+++ src/app/site-information/site-information.module.ts
@@ -1,23 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { SiteInformationRoutingModule } from './site-information-routing.routes';
-import { SiteInformationRouringComponent } from './components/site-information-rouring/site-information-rouring.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { SharedModule } from '../shared/shared.module';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-
-
+import { SiteInformationRoutingModule } from "./site-information-routing.routes";
+import { SiteInformationRouringComponent } from "./components/site-information-rouring/site-information-rouring.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { SharedModule } from "../shared/shared.module";
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
+import { BlogComponent } from "./components/blog/blog.component";
 
 @NgModule({
   declarations: [
-    SiteInformationRouringComponent, 
-    AboutUsComponent, ContactUsComponent
+    SiteInformationRouringComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    BlogComponent
   ],
-  imports: [
-    CommonModule,
-    SiteInformationRoutingModule,
-    SharedModule
-  ]
+  imports: [CommonModule, SiteInformationRoutingModule, SharedModule]
 })
-export class SiteInformationModule { }
+export class SiteInformationModule {}
