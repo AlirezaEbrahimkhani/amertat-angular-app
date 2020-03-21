531702687c8a0ec0b0f56a3f93267a2a3936cac6
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-21 16:47:56 +0430

    csi :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,57 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
-import { HeaderFooterModule } from './header-footer/header-footer.module';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SharedModule } from './shared/shared.module';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./shared/material.module";
+import { HeaderFooterModule } from "./header-footer/header-footer.module";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { SharedModule } from "./shared/shared.module";
 
-
-const routes : Routes = [
+const routes: Routes = [
   {
-    path : "",
-    component : HomeComponent,
-    children : [
+    path: "",
+    component: HomeComponent,
+    children: [
       {
-        path : "medical-service",
-        loadChildren : "./medical-service/medical-service.module#MedicalServiceModule"
+        path: "medical-service",
+        loadChildren:
+          "./medical-service/medical-service.module#MedicalServiceModule"
       },
       {
-        path : "information",
-        loadChildren : "./information/information.module#InformationModule"
+        path: "information",
+        loadChildren: "./information/information.module#InformationModule"
       },
       {
-        path : "tourism-service",
-        loadChildren : "./tourism-service/tourism-service.module#TourismServiceModule"
+        path: "tourism-service",
+        loadChildren:
+          "./tourism-service/tourism-service.module#TourismServiceModule"
       },
       {
-        path : "site-information",
-        loadChildren : "./site-information/site-information.module#SiteInformationModule"
+        path: "site-information",
+        loadChildren:
+          "./site-information/site-information.module#SiteInformationModule"
       }
     ]
   },
   {
-    path : "**",
-    redirectTo : ""
+    path: "**",
+    redirectTo: ""
   }
-]
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     HeaderFooterModule,
     RouterModule.forRoot(routes),
-    SharedModule,
+    SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
