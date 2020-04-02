124c0bd34c17f24c6808de6dde308ffc9e1983c9
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-02 13:55:06 +0430

    accommodation service component created

---
+++ src/app/tourism-service/components/accommodation-service/accommodation-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation-service',
+  templateUrl: './accommodation-service.component.html',
+  styleUrls: ['./accommodation-service.component.scss']
+})
+export class AccommodationServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tourism-service/tourism-service-routing.routes.ts
+++ src/app/tourism-service/tourism-service-routing.routes.ts
@@ -1,13 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TourismServiceRoutingComponent } from './components/tourism-service-routing/tourism-service-routing.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TourismServiceRoutingComponent } from "./components/tourism-service-routing/tourism-service-routing.component";
+import { TourismServiceComponent } from "./components/tourism-service/tourism-service.component";
+import { AccommodationServiceComponent } from "./components/accommodation-service/accommodation-service.component";
 
 const routes: Routes = [
   {
-    path : "",
-    component : TourismServiceRoutingComponent,
-    children : []
+    path: "",
+    component: TourismServiceRoutingComponent,
+    children: [
+      {
+        path: "tourism-services",
+        component: TourismServiceComponent,
+        data: {
+          breadcrumb: "خدمات گردشگری"
+        }
+      },
+      {
+        path: "accommodation-services",
+        component: AccommodationServiceComponent,
+        data: {
+          breadcrumb: "خدمات اسکان"
+        }
+      }
+    ]
   }
 ];
 
@@ -15,4 +31,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TourismServiceRoutingModule { }
+export class TourismServiceRoutingModule {}
