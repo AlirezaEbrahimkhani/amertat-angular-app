7e067315face696e4693a8b22be7c95f9f963027
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-02 21:37:03 +0430

    fix some bug

--- src/app/shared/components/hospitals-card/hospitals-card.component.ts
+++ src/app/shared/components/hospitals-card/hospitals-card.component.ts
@@ -6,32 +6,7 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./hospitals-card.component.scss"]
 })
 export class HospitalsCardComponent implements OnInit {
-  @Input() items: any[] = [
-    {
-      name: "Shahid Beheshti",
-      desctrption: "ssbthe dndne dnetbgdf gehyjr  gtge kmfgb sgbet dhnfhy dnty",
-      imgSrc:
-        "../../../../assets/pictures/hospitals/adhy-savala-zbpgmGe27p8-unsplash.jpg"
-    },
-    {
-      name: "Shahid Beheshti",
-      desctrption: "ssbthe dndne dnetbgdf gehyjr  gtge kmfgb sgbet dhnfhy dnty",
-      imgSrc:
-        "../../../../assets/pictures/hospitals/esaias-tan-JeWyWRMH4rQ-unsplash.jpg"
-    },
-    {
-      name: "Shahid Beheshti",
-      desctrption: "ssbthe dndne dnetbgdf gehyjr  gtge kmfgb sgbet dhnfhy dnty",
-      imgSrc:
-        "../../../../assets/pictures/hospitals/martha-dominguez-de-gouveia-nMyM7fxpokE-unsplash.jpg"
-    },
-    {
-      name: "Shahid Beheshti",
-      desctrption: "ssbthe dndne dnetbgdf gehyjr  gtge kmfgb sgbet dhnfhy dnty",
-      imgSrc:
-        "../../../../assets/pictures/hospitals/adhy-savala-zbpgmGe27p8-unsplash.jpg"
-    }
-  ];
+  @Input() items: any[] = [];
 
   constructor() {}
 
