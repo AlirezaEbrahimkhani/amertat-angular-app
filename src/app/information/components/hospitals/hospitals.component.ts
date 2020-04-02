db2f7cc469851c93093fde3133bb469bfd14f8a4
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-02 13:31:10 +0430

    hospitals component created

---
+++ src/app/information/components/hospitals/hospitals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospitals',
+  templateUrl: './hospitals.component.html',
+  styleUrls: ['./hospitals.component.scss']
+})
+export class HospitalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/information/information.module.ts
+++ src/app/information/information.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { InformationRoutingModule } from './information-routing.routes';
 import { InformationRoutingComponent } from './components/information-routing/information-routing.component';
+import { HospitalsComponent } from './components/hospitals/hospitals.component';
+import { HospitalComponent } from './components/hospitals/hospital/hospital.component';
 
 
 @NgModule({
-  declarations: [InformationRoutingComponent],
+  declarations: [InformationRoutingComponent, HospitalsComponent, HospitalComponent],
   imports: [
     CommonModule,
     InformationRoutingModule
