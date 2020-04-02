68de1986a4dc1d6ea124e8bf4d69246dbad326ca
alireza <alirezaya412@gmail.com>
2020-04-02 23:40:03 +0430

    first

--- src/app/information/components/doctors/doctors.component.ts
+++ src/app/information/components/doctors/doctors.component.ts
@@ -6,6 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctors.component.scss']
 })
 export class DoctorsComponent implements OnInit {
+  doctors: any[] = [
+    {
+      name: "alireza yoosefi",
+      proficiency: "Health",
+      description: "More than 10 years exprience and ...",
+      imgSrc: "../../../../assets/About Us/person4.jpg",
+      degree: "09998883434",
+    },
+    {
+      name: "alireza yoosefi",
+      proficiency: "Health",
+      description: "More than 10 years exprience and ...",
+      imgSrc: "../../../../assets/About Us/person4.jpg",
+      degree: "09998883434",
+    },
+    {
+      name: "alireza yoosefi",
+      proficiency: "Health",
+      description: "More than 10 years exprience and ...",
+      imgSrc: "../../../../assets/About Us/person4.jpg",
+      degree: "09998883434",
+    },
+    {
+      name: "alireza yoosefi",
+      proficiency: "Health",
+      description: "More than 10 years exprience and ...",
+      imgSrc: "../../../../assets/About Us/person4.jpg",
+      degree: "09998883434",
+    },
+    {
+      name: "alireza yoosefi",
+      proficiency: "Health",
+      description: "More than 10 years exprience and ...",
+      imgSrc: "../../../../assets/About Us/person4.jpg",
+      degree: "09998883434",
+    },
+  ];
 
   constructor() { }
 
