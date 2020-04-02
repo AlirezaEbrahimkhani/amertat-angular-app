8e935676f4bf06e716624d206dfd23fd9a75c7b0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-02 13:31:46 +0430

    doctors component created

--- src/app/information/information.module.ts
+++ src/app/information/information.module.ts
@@ -5,10 +5,12 @@ import { InformationRoutingModule } from './information-routing.routes';
 import { InformationRoutingComponent } from './components/information-routing/information-routing.component';
 import { HospitalsComponent } from './components/hospitals/hospitals.component';
 import { HospitalComponent } from './components/hospitals/hospital/hospital.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { DoctorComponent } from './components/doctors/doctor/doctor.component';
 
 
 @NgModule({
-  declarations: [InformationRoutingComponent, HospitalsComponent, HospitalComponent],
+  declarations: [InformationRoutingComponent, HospitalsComponent, HospitalComponent, DoctorsComponent, DoctorComponent],
   imports: [
     CommonModule,
     InformationRoutingModule
