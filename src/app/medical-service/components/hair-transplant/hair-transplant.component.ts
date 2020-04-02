2fa34a1bcfc791752ca47fdad81164e224b64795
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-02 13:40:23 +0430

    medical service components created

---
+++ src/app/medical-service/components/hair-transplant/hair-transplant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hair-transplant',
+  templateUrl: './hair-transplant.component.html',
+  styleUrls: ['./hair-transplant.component.scss']
+})
+export class HairTransplantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/medical-service/medical-service.module.ts
+++ src/app/medical-service/medical-service.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from "@angular/common";
 
 import { MedicalServiceRoutingComponent } from "./components/medical-service-routing/medical-service-routing.component";
 import { MedicalServiceRoutingModule } from "./medical-service-routing.routes";
+import { LiposuctionComponent } from './components/liposuction/liposuction.component';
+import { HairTransplantComponent } from './components/hair-transplant/hair-transplant.component';
 
 @NgModule({
-  declarations: [MedicalServiceRoutingComponent],
+  declarations: [MedicalServiceRoutingComponent, LiposuctionComponent, HairTransplantComponent],
   imports: [CommonModule, MedicalServiceRoutingModule]
 })
 export class MedicalServiceModule {}
