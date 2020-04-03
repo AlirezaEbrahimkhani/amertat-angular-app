5281b6a2621e99dd0abd425676a89a9c5c9ba5ad
alireza <alirezaya412@gmail.com>
2020-04-04 00:10:33 +0430

    create carousel module

---
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+  @Input() items : any[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
