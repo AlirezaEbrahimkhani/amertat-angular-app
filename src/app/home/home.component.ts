733059f9dcee1bd61f2031cc365751c5f43ce09f
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-04 02:06:41 +0430

    header completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  imgSrc: string = "./../../assets/home/header3.jpg";
+  title: string = "Amertat Medical Tourism is the best place for treatment";
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  changeRoute() {
+    this.router.navigate(["/amertat/site-information/contact-us"]);
   }
-
 }
