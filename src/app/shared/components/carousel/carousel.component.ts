db93ebacaba973705ed0cb2eb0fc268f7f0f329b
alireza <alirezaya412@gmail.com>
2020-04-04 00:23:10 +0430

    carousel style

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  @Input() items : any[] = [];
+  @Input() items : any[] = [
+    {
+      name: "Light mask",
+      description: "First text",
+      imgSrc: "./../../../../../assets/pictures/others/img (6).jpg",
+    },
+    {
+      name: "Strong mask",
+      description: "Secondary text",
+      imgSrc: "./../../../../../assets/pictures/others/img (9).jpg",
+    },
+    {
+      name: "Slight mask",
+      description: "Third text",
+      imgSrc: "./../../../../../assets/pictures/others/img (68).jpg",
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
