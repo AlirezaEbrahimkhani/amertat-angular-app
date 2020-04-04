50ca7e753c2f0bff1dd56613da05a8a3c584f230
alireza <alirezaya412@gmail.com>
2020-04-05 01:20:28 +0430

    first

--- src/app/information/components/cities/city/city.component.ts
+++ src/app/information/components/cities/city/city.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-city',
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city.component.scss']
 })
 export class CityComponent implements OnInit {
+
+  @Input() city: any = {
+    name: "Zanjan",
+    mainImsSrc: "../../../../../assets/tourism-service/city4.jpg",
+    carouselItems : [
+      {
+        name: "Light mask",
+        description: "First text",
+        imgSrc: "./../../../../../assets/pictures/others/img (6).jpg",
+      },
+      {
+        name: "Strong mask",
+        description: "Secondary text",
+        imgSrc: "./../../../../../assets/pictures/others/img (9).jpg",
+      },
+      {
+        name: "Slight mask",
+        description: "Third text",
+        imgSrc: "./../../../../../assets/pictures/others/img (68).jpg",
+      },
+    ],
+  }
 
   constructor() { }
 
