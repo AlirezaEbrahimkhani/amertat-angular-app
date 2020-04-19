00a05a4e7aa7cc63576c5597534b7e28fb9275e0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-04-19 23:43:54 +0430

    csi :)

--- src/app/header-footer/components/header/header.component.ts
+++ src/app/header-footer/components/header/header.component.ts
@@ -1,36 +1,31 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { MatDialog, MatButton } from '@angular/material';
-import { HeaderDialogComponent } from './header-dialog/header-dialog.component';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { MatDialog, MatButton } from "@angular/material";
+import { HeaderDialogComponent } from "./header-dialog/header-dialog.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild("contact", { static: true }) contactBtn: ElementRef<MatButton>;
+  @ViewChild("start", { static: true }) startBtn: ElementRef<MatButton>;
+  @ViewChild("qoute", { static: true }) qouteBtn: ElementRef<MatButton>;
 
-  @ViewChild('contact', { static: true }) contactBtn: ElementRef<MatButton>;
-  @ViewChild('start', { static: true }) startBtn: ElementRef<MatButton>;
-  @ViewChild('qoute', { static: true }) qouteBtn: ElementRef<MatButton>;
+  constructor(public matdialog: MatDialog) {}
 
-  constructor(public dialog : MatDialog) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  openModal(type : string){
-    const dialog = this.dialog.open(HeaderDialogComponent, {
+  openModal(type: string) {
+    const dialog = this.matdialog.open(HeaderDialogComponent, {
       width: "700px",
-      height: "600px"
+      height: "600px",
     });
-    if(type == "contact")
-      dialog.componentInstance.type = "contact";
-    else if(type == "start")
-      dialog.componentInstance.type = "start";
-    else if(type == "qoute")
-      dialog.componentInstance.type = "qoute";
+    if (type == "contact") dialog.componentInstance.type = "contact";
+    else if (type == "start") dialog.componentInstance.type = "start";
+    else if (type == "qoute") dialog.componentInstance.type = "qoute";
 
-    dialog.afterClosed().subscribe(result => {
+    dialog.afterClosed().subscribe((result) => {
       (this.contactBtn as any)._getHostElement().blur();
       (this.startBtn as any)._getHostElement().blur();
       (this.qouteBtn as any)._getHostElement().blur();
