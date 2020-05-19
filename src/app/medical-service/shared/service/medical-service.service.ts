d74afaaa3b9014e4e7b2692a2f85bef680f10b6d
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-19 16:01:21 +0430

    API added to service

--- src/app/medical-service/shared/service/medical-service.service.ts
+++ src/app/medical-service/shared/service/medical-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicalServiceService {
 
-  constructor() { }
+  baseUrl = "http://195.206.106.154:3000/"
+
+  constructor(private http : HttpClient) { }
+
+  getLiposuctionPage(){
+    return this.http.get(this.baseUrl + "lisposuction-page");
+  }
+
+  getHairTranspalentPage(){
+    return this.http.get(this.baseUrl + "hair-transaction-page");
+  }
 }
