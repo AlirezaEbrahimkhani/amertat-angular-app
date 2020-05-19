c33cc09f272b02df2a1dfdb5ab70abfc784a86af
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-19 16:01:44 +0430

    connnect components to api

--- src/app/medical-service/components/hair-transplant/hair-transplant.component.ts
+++ src/app/medical-service/components/hair-transplant/hair-transplant.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MedicalServiceService } from "../../shared/service/medical-service.service";
 
 @Component({
-  selector: 'app-hair-transplant',
-  templateUrl: './hair-transplant.component.html',
-  styleUrls: ['./hair-transplant.component.scss']
+  selector: "app-hair-transplant",
+  templateUrl: "./hair-transplant.component.html",
+  styleUrls: ["./hair-transplant.component.scss"],
 })
 export class HairTransplantComponent implements OnInit {
+  pageConfig: any = [];
 
-  constructor() { }
+  constructor(private medicalSrv: MedicalServiceService) {}
 
   ngOnInit() {
+    this.medicalSrv.getHairTranspalentPage().subscribe((response: any[]) => {
+      this.pageConfig = response[0];
+    });
   }
-
 }
--- src/app/medical-service/components/liposuction/liposuction.component.ts
+++ src/app/medical-service/components/liposuction/liposuction.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MedicalServiceService } from "../../shared/service/medical-service.service";
 
 @Component({
-  selector: 'app-liposuction',
-  templateUrl: './liposuction.component.html',
-  styleUrls: ['./liposuction.component.scss']
+  selector: "app-liposuction",
+  templateUrl: "./liposuction.component.html",
+  styleUrls: ["./liposuction.component.scss"],
 })
 export class LiposuctionComponent implements OnInit {
+  pageConfig: any = [];
 
-  constructor() { }
+  constructor(private medicalSrv: MedicalServiceService) {}
 
   ngOnInit() {
+    this.medicalSrv.getLiposuctionPage().subscribe((response: any[]) => {
+      this.pageConfig = response[0];
+    });
   }
-
 }
