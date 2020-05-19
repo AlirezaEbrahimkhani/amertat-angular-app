aa2d48f99b3e2fcea26f6fc1174e1c4dd720e45f
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-19 20:20:31 +0430

    get doctor data

--- src/app/information/shared/information.service.ts
+++ src/app/information/shared/information.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class InformationService {
+  baseUrl = "http://195.206.106.154:3000/";
 
-  baseUrl = "http://195.206.106.154:3000/"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) { }
-
-  getDoctorsPage(){
+  getDoctorsPage() {
     return this.http.get(this.baseUrl + "doctors-page");
   }
 
-  getDoctors(){
+  getDoctors() {
     return this.http.get(this.baseUrl + "doctor");
+  }
+
+  getDoctor(id) {
+    return this.http.get(this.baseUrl + "doctor/" + id);
   }
 }
