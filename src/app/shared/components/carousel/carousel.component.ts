0dc5d4b6dcd67ee96b13cd623f02c67f5c7d07f2
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-19 20:21:11 +0430

    carousel component data binding

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CarouselComponent implements OnInit {
   @Input() title: string = "Carousel";
   @Input() items : any[] = [];
+  baseUrl = "http://195.206.106.154:3000"
   constructor() { }
 
   ngOnInit() {
