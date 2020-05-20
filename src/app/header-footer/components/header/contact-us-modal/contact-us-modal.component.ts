13d17274769ae707845bdc441ebd9b6a40fefbb8
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-20 22:26:45 +0430

    form and validation

--- src/app/header-footer/components/header/contact-us-modal/contact-us-modal.component.ts
+++ src/app/header-footer/components/header/contact-us-modal/contact-us-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialogRef } from "@angular/material";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-contact-us-modal",
@@ -8,17 +9,31 @@ import { MatDialogRef } from "@angular/material";
 })
 export class ContactUsModalComponent implements OnInit {
   feedBacks: string[] = [
-    "haji namosan karet doroste = 5",
-    "haji karet doroste = 4",
-    "haji karet okeye = 3",
-    "haji karet yekam gir dare = 2",
-    "haji razi nistam = 1",
-    "haji namosan gand zadi = 0",
+    "Very Good = 5",
+    "Good = 4",
+    "Not Good Not Bad Just Fine = 3",
+    "Bad = 2",
+    "Very Bad = 1",
+    "Awful = 0",
   ];
+  form = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    family: new FormControl("", [Validators.required]),
+    phoneNumber: new FormControl("", [Validators.required]),
+    feedBack: new FormControl("", [Validators.required]),
+    subject: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    address: new FormControl("", [
+      Validators.required,
+      Validators.maxLength(200),
+    ]),
+  });
 
   constructor(public dialogRef: MatDialogRef<ContactUsModalComponent>) {}
 
   ngOnInit() {}
+
+  onSubmit() {}
 
   onCancel() {
     this.dialogRef.close();
--- src/app/header-footer/header-footer.module.ts
+++ src/app/header-footer/header-footer.module.ts
@@ -1,27 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderDialogComponent } from './components/header/header-dialog/header-dialog.component';
-import { ContactUsModalComponent } from './components/header/contact-us-modal/contact-us-modal.component';
-import { MaterialModule } from '../shared/material.module';
-import { GetStartedModalComponent } from './components/header/get-started-modal/get-started-modal.component';
-import { GetFreeQouteModalComponent } from './components/header/get-free-qoute-modal/get-free-qoute-modal.component';
-import { NavbarModule, WavesModule, ButtonsModule, MdbIconComponent, IconsModule, DropdownModule } from 'angular-bootstrap-md'
-import { BrowserModule } from '@angular/platform-browser';
-import { HelpButtonComponent } from './components/help-button/help-button.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HeaderDialogComponent } from "./components/header/header-dialog/header-dialog.component";
+import { ContactUsModalComponent } from "./components/header/contact-us-modal/contact-us-modal.component";
+import { MaterialModule } from "../shared/material.module";
+import { GetStartedModalComponent } from "./components/header/get-started-modal/get-started-modal.component";
+import { GetFreeQouteModalComponent } from "./components/header/get-free-qoute-modal/get-free-qoute-modal.component";
+import {
+  NavbarModule,
+  WavesModule,
+  ButtonsModule,
+  IconsModule,
+  DropdownModule,
+} from "angular-bootstrap-md";
+import { BrowserModule } from "@angular/platform-browser";
+import { HelpButtonComponent } from "./components/help-button/help-button.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    HeaderComponent, 
-    FooterComponent, 
-    NavbarComponent, 
-    HeaderDialogComponent, 
-    ContactUsModalComponent, 
-    GetStartedModalComponent, 
-    GetFreeQouteModalComponent, HelpButtonComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeaderDialogComponent,
+    ContactUsModalComponent,
+    GetStartedModalComponent,
+    GetFreeQouteModalComponent,
+    HelpButtonComponent,
   ],
   imports: [
     CommonModule,
@@ -31,16 +39,16 @@ import { HelpButtonComponent } from './components/help-button/help-button.compon
     ButtonsModule,
     IconsModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     DropdownModule.forRoot(),
   ],
-  exports : [
-    HeaderComponent, 
-    FooterComponent, 
+  exports: [
+    HeaderComponent,
+    FooterComponent,
     NavbarComponent,
     HelpButtonComponent,
   ],
-  entryComponents : [
-    HeaderDialogComponent,
-  ]
+  entryComponents: [HeaderDialogComponent],
 })
-export class HeaderFooterModule { }
+export class HeaderFooterModule {}
