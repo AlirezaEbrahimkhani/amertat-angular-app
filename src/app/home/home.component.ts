18492164f3f99e8e492225d579abbb598417e419
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-20 21:14:52 +0430

    bind data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { SiteInformationService } from "../site-information/shared/services/site-information.service";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
   imgSrc: string = "./../../assets/home/header3.jpg";
   title: string = "Amertat Medical Tourism is the best place for treatment";
+  baseUrl: string = "http://195.206.106.154:3000";
+  counter: number = 0;
 
-  blogPostNumber: string[] = ["One", "Two", "Three", "Four", "Five", "Six"];
+  blogCard: any[] = [];
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private siteInfoSrv: SiteInformationService
+  ) {}
 
-  ngOnInit() {}
-
-  changeRoute() {
-    this.router.navigate(["/amertat/site-information/contact-us"]);
+  ngOnInit() {
+    this.siteInfoSrv.getBlog().subscribe((response: any[]) => {
+      response.forEach((element) => {
+        if (this.counter != 9) {
+          this.blogCard.push(element);
+          this.counter++;
+        }
+      });
+    });
   }
 
-  onClickCard() {
-    window.open("https://www.health.harvard.edu/blog/");
+  changeRoute() {
+    this.router.navigate(["/amertat/site-information/about-us"]);
+  }
+
+  onClickCard(item) {
+    window.open(item.link);
   }
 }
