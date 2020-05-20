3e33e127ee7eb9ce2fed38a1a1c5886a5560ebca
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-20 21:40:11 +0430

    data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { SiteInformationService } from "../site-information/shared/services/site-information.service";
+import { HomeService } from "./home.service";
 
 @Component({
   selector: "app-home",
@@ -12,12 +13,14 @@ export class HomeComponent implements OnInit {
   title: string = "Amertat Medical Tourism is the best place for treatment";
   baseUrl: string = "http://195.206.106.154:3000";
   counter: number = 0;
+  pageConfig: any = [];
 
   blogCard: any[] = [];
 
   constructor(
     private router: Router,
-    private siteInfoSrv: SiteInformationService
+    private siteInfoSrv: SiteInformationService,
+    private homeSrv: HomeService
   ) {}
 
   ngOnInit() {
@@ -26,6 +29,14 @@ export class HomeComponent implements OnInit {
         if (this.counter != 9) {
           this.blogCard.push(element);
           this.counter++;
+        }
+      });
+    });
+
+    this.homeSrv.getHomePage().subscribe((response: any[]) => {
+      response.forEach((element) => {
+        if (element.isActive) {
+          this.pageConfig = element;
         }
       });
     });
