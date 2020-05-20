38ac99474f1bcf15c6c71a6cc761b6bd0c387be3
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-20 20:38:15 +0430

    fetch data

--- src/app/site-information/shared/services/site-information.service.ts
+++ src/app/site-information/shared/services/site-information.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SiteInformationService {
+  baseUrl = "http://195.206.106.154:3000/";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getBlog() {
+    return this.http.get(this.baseUrl + "blog");
+  }
 }
