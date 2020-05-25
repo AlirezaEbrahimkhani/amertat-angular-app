af46e5d9af39efe1e05a7f9a242cb44b0a731864
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-05-25 23:36:48 +0430

    add service and bind data

--- src/app/shared/components/hospitals-card/hospitals-card.component.ts
+++ src/app/shared/components/hospitals-card/hospitals-card.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-hospitals-card",
   templateUrl: "./hospitals-card.component.html",
-  styleUrls: ["./hospitals-card.component.scss"]
+  styleUrls: ["./hospitals-card.component.scss"],
 })
 export class HospitalsCardComponent implements OnInit {
   @Input() items: any[] = [];
+  baseUrl = "http://195.206.106.154:3000";
 
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
-  onChangeRoute() {
-    this.router.navigate(["/amertat/information/hospitals/hospital"]);
+  onChangeRoute(item) {
+    this.router.navigate(
+      [`/amertat/information/hospitals/hospital/${item.id}`],
+      {
+        queryParams: item.id,
+      }
+    );
   }
 }
