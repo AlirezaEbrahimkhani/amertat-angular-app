d69270200ae87f9575db0d72715819352812a6bc
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-23 18:22:01 +0430

    about us page API connected

--- src/app/site-information/components/about-us/about-us.component.ts
+++ src/app/site-information/components/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SiteInformationService } from "../../shared/services/site-information.service";
 
 @Component({
   selector: "app-about-us",
@@ -6,68 +7,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./about-us.component.scss"],
 })
 export class AboutUsComponent implements OnInit {
-  lines: string[] = [
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit.Cupiditate, iure incidunt aspernatur officiis perferendis repudiandae.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit.Cupiditate, iure incidunt aspernatur officiis perferendis repudiandae.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit.Cupiditate, iure incidunt aspernatur officiis perferendis repudiandae.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit.Cupiditate, iure incidunt aspernatur officiis perferendis repudiandae.",
-  ];
+  persons: any[] = [];
 
-  persons: any[] = [
-    {
-      name: "Alireza Ebrahimkhani",
-      position: "Project Manager",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi nostrum, ab libero voluptas officia.",
-      img: "person4.jpg",
-    },
-    {
-      name: "Susan Williams",
-      position: "Accounting Desk Person",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi nostrum, ab libero voluptas officia.",
-      img: "person2.jpg",
-    },
-    {
-      name: "John doe",
-      position: "Lead Writer",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi nostrum, ab libero voluptas officia.",
-      img: "person3.jpg",
-    },
-    {
-      name: "Kate Hliznitsova",
-      position: "Secretary  ",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi nostrum, ab libero voluptas officia.",
-      img: "person1.jpg",
-    },
-  ];
+  developmentTeam: any[] = [];
 
-  developmentTeam: any[] = [
-    {
-      imgSrc: "../../../../assets/contact-us/person2.jpg",
-      name: "Alireza Ebrahimkhani",
-      label: "Project Manager",
-    },
-    {
-      imgSrc: "../../../../assets/contact-us/person4.jpg",
-      name: "Josef seed",
-      label: "Web Developer",
-    },
-    {
-      imgSrc: "../../../../assets/contact-us/person3.jpg",
-      name: "John Doe",
-      label: "Web Developer",
-    },
-    {
-      imgSrc: "../../../../assets/contact-us/person1.jpg",
-      name: "Steve Smith",
-      label: "Web Developer",
-    },
-  ];
+  constructor(private siteInfoSrv: SiteInformationService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.siteInfoSrv.getTeams().subscribe((response: any) => {
+      const { data } = response;
+      this.persons = data;
+    });
 
-  ngOnInit() {}
+    this.siteInfoSrv.getDevelopers().subscribe((response: any) => {
+      const { data } = response;
+      this.developmentTeam = data;
+    });
+  }
 }
--- src/app/site-information/shared/services/site-information.service.ts
+++ src/app/site-information/shared/services/site-information.service.ts
@@ -5,11 +5,19 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root",
 })
 export class SiteInformationService {
-  baseUrl = "http://195.206.106.154:3000/";
+  baseUrl = "http://localhost:5000/api/";
 
   constructor(private http: HttpClient) {}
 
   getBlog() {
-    return this.http.get(this.baseUrl + "blog");
+    return this.http.get("http://195.206.106.154:3000/" + "blog");
+  }
+
+  getTeams() {
+    return this.http.get(this.baseUrl + "team");
+  }
+
+  getDevelopers() {
+    return this.http.get(this.baseUrl + "developers");
   }
 }
